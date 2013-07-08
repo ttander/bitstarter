d387e04e2f8fd26545e9ac15fcb4508772800d3c
ttander <ttander@gmail.com>
2013-07-08 21:04:35 +0000

    web.js

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
     var fs = require('fs');
 
-    fs.readFile('Index.html', function read(err, data) {
+    fs.readFile('./Index.html', function read(err, data) {
 	if (err) {
             throw err;
 	}
